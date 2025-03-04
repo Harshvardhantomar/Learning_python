101aa171887dff5ec825d70629724b19065a8ce8
Harshvardhantomar <harshvardhant789@gmail.com>
2025-03-04 21:11:30 +0530

    Starting with Chapter 6 Dictonaries

---
+++ Chapter_5/practice.py
@@ -0,0 +1,15 @@
+a = ["he","Ho"]
+
+b = ("he","ho")
+
+# print(a.sort())
+# print(b.sort())
+
+# print(a.reverse())
+# print(b.reverse())
+
+# print(a.append("a"))
+# print(b.append("a"))
+
+print(a.pop())
+print(b.pop())
